12d8ccfd9055d48d3652b57a5b83b95dc082dea7
Leandro Rodrigues <lbrguitar@gmail.com>
2021-01-30 20:46:28 -0500

    Removed debug calls and added enemy customizable parameters.

--- Greed/Assets/Scripts/EnemyController.cs
+++ Greed/Assets/Scripts/EnemyController.cs
@@ -9,16 +9,26 @@ public class EnemyController : MonoBehaviour
     private static readonly string playerTag = "Player";
 
     [SerializeField]
+    private int baseHealth = 100;
+    [SerializeField]
+    private int baseDamage = 5;
+    [SerializeField]
+    private float attackDistance = 5.0f;
+    [SerializeField]
+    private float attackCooldown = 1.0f;
+    [SerializeField]
     private Transform goal;
 
     private NavMeshAgent navMeshAgent;
     private bool playerInArea;
+    private int health;
 
     // Start is called before the first frame update
     void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         // Other setup if necessary
+        health = baseHealth;
     }
 
     // Update is called once per frame
@@ -29,14 +39,20 @@ public class EnemyController : MonoBehaviour
             // Raycast to see if can see player
             RaycastHit hit;
             Vector3 rayDirection = goal.position - transform.position;
-            Debug.DrawLine(transform.position, goal.position);
-            if (Physics.Raycast(transform.position, rayDirection.normalized, out hit, 100))
+            if(Physics.Raycast(transform.position, rayDirection.normalized, out hit, rayDirection.magnitude))
             {
-                Debug.Log("Hit something");
                 if(hit.transform.gameObject.tag == playerTag)
                 {
                     navMeshAgent.destination = goal.position;
                 }
+            }
+
+            // Check if player is close enough to be attacked
+            if(rayDirection.magnitude <= attackDistance)
+            {
+                // attack player
+                // TODO: implement proper attack logic
+                Debug.Log("Attack Player");
             }
         }
     }
@@ -57,5 +73,19 @@ public class EnemyController : MonoBehaviour
             playerInArea = false;
             navMeshAgent.isStopped = true;
         }
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void GiveHealth(int value)
+    {
+        health += value;
     }
 }
