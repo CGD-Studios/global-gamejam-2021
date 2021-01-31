09f0026946ecf455425238ea7421dcf157dde21e
Mauricio Beltran Franco <mauricio.beltranfranco@live.fr>
2021-01-31 16:07:31 -0500

    Sound and Music
    - Added sounds and music (incomplete)

--- Greed/Assets/Scripts/MainMenu.cs
+++ Greed/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame() 
     {
+        SoundManager.PlayRoaming();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
