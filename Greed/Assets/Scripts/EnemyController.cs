415f2ee83c9d9555b0fe436c70af205319cecc94
Leandro Rodrigues <lbrguitar@gmail.com>
2021-01-30 20:35:55 -0500

    Enemy follows player if player is on sight.

--- Greed/Assets/Scripts/EnemyController.cs
+++ Greed/Assets/Scripts/EnemyController.cs
@@ -3,27 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent), typeof(SphereCollider))]
 public class EnemyController : MonoBehaviour
 {
+    private static readonly string playerTag = "Player";
+
     [SerializeField]
     private Transform goal;
 
     private NavMeshAgent navMeshAgent;
+    private bool playerInArea;
 
     // Start is called before the first frame update
     void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-    }
-
-    void Start()
-    {
-        navMeshAgent.destination = goal.position;
+        // Other setup if necessary
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(playerInArea)
+        {
+            // Raycast to see if can see player
+            RaycastHit hit;
+            Vector3 rayDirection = goal.position - transform.position;
+            Debug.DrawLine(transform.position, goal.position);
+            if (Physics.Raycast(transform.position, rayDirection.normalized, out hit, 100))
+            {
+                Debug.Log("Hit something");
+                if(hit.transform.gameObject.tag == playerTag)
+                {
+                    navMeshAgent.destination = goal.position;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == playerTag)
+        {
+            playerInArea = true;
+            navMeshAgent.isStopped = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == playerTag)
+        {
+            playerInArea = false;
+            navMeshAgent.isStopped = true;
+        }
     }
 }
